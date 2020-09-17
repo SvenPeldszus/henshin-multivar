c997d82c9799e8504f7dd0cb351471421fb1903b
Sven Peldszus <speldszus@uni-koblenz.de>
2020-09-17 12:15:32 +0200

    Maintenance Removed unsused dependencies Weakened too restrictive parameter types: e.g. Map istead of HashMap
    Change-Id: Ie13bffd9b395b68f48ea74e4ca3080c15e3d2c6b

--- plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/info/ConditionInfo.java
+++ plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/info/ConditionInfo.java
@@ -1,8 +1,8 @@
 /**
  * <copyright>
- * Copyright (c) 2010-2014 Henshin developers. All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Eclipse Public License v1.0 which 
+ * Copyright (c) 2010-2014 Henshin developers. All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * </copyright>
@@ -21,26 +21,26 @@ import org.eclipse.emf.henshin.model.Parameter;
 import org.eclipse.emf.henshin.model.Rule;
 
 public class ConditionInfo {
-	
-	private Map<String, Collection<String>> conditionParameters;
-	
+
+	private final Map<String, Collection<String>> conditionParameters;
+
 	public ConditionInfo(Rule rule) {
-		this.conditionParameters = new HashMap<String, Collection<String>>();
-		
-		Collection<String> parameterNames = new ArrayList<String>();
+		this.conditionParameters = new HashMap<>();
+
+		Collection<String> parameterNames = new ArrayList<>();
 		for (Parameter parameter : rule.getParameters()) {
 			parameterNames.add(parameter.getName());
 		}
-		
+
 		for (AttributeCondition condition : rule.getAttributeConditions()) {
 			String conditionText = condition.getConditionText();
 			Collection<String> usedParameters = extractParameter(conditionText, parameterNames);
-			conditionParameters.put(conditionText, usedParameters);
+			this.conditionParameters.put(conditionText, usedParameters);
 		}
 	}
-	
+
 	private Collection<String> extractParameter(String testString, Collection<String> parameterNames) {
-		Collection<String> usedParameters = new HashSet<String>();
+		Collection<String> usedParameters = new HashSet<>();
 		StringTokenizer quoteParser = new StringTokenizer(testString, "\"\'");
 		while (quoteParser.hasMoreElements()) {
 			String nonQuotedString = quoteParser.nextToken();
@@ -54,19 +54,20 @@ public class ConditionInfo {
 				}
 			}
 			// discard the quoted part
-			if (quoteParser.hasMoreElements())
+			if (quoteParser.hasMoreElements()) {
 				quoteParser.nextElement();
+			}
 		}
-		
+
 		return usedParameters;
 	}
-	
+
 	/**
 	 * Get the condition parameters. This is a map from condition tests to
 	 * collections of used parameters in the condition texts.
 	 * @return the conditionParameters The condition parameters.
 	 */
 	public Map<String, Collection<String>> getConditionParameters() {
-		return conditionParameters;
+		return this.conditionParameters;
 	}
 }
--- plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/matching/conditions/ConditionHandler.java
+++ plugins/org.eclipse.emf.henshin.interpreter/src/org/eclipse/emf/henshin/interpreter/matching/conditions/ConditionHandler.java
@@ -1,8 +1,8 @@
 /**
  * <copyright>
- * Copyright (c) 2010-2014 Henshin developers. All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Eclipse Public License v1.0 which 
+ * Copyright (c) 2010-2014 Henshin developers. All rights reserved.
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * </copyright>
@@ -11,60 +11,70 @@ package org.eclipse.emf.henshin.interpreter.matching.conditions;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 import javax.script.ScriptEngine;
 
+import org.eclipse.emf.henshin.model.util.ScriptEngineWrapper;
+
 /**
  * Condition handler.
- * 
+ *
  * @author Enrico Biermann, Christian Krause
  */
 public class ConditionHandler {
-	
+
 	// Attribute conditions:
 	final Collection<AttributeCondition> attributeConditions;
-	
+
 	// Involved conditions:
 	final Map<String, Collection<AttributeCondition>> involvedConditions;
-	
+
 	// Assigned parameters:
 	final Collection<String> assignedParameters;
 
 	// Used script engine:
-	final ScriptEngine scriptEngine;
+	final ScriptEngineWrapper scriptEngine;
 
 	/**
 	 * Default constructor.
 	 * @param conditionParameters Condition parameters.
 	 * @param scriptEngine Script engine,
+	 * @param localJavaImports
+	 * @param javaImports
 	 */
 	public ConditionHandler(
 			Map<String, Collection<String>> conditionParameters,
-			ScriptEngine scriptEngine) {
-		
-		this.attributeConditions = new ArrayList<AttributeCondition>();
-		this.involvedConditions = new HashMap<String, Collection<AttributeCondition>>();
-		this.assignedParameters = new HashSet<String>();
+			ScriptEngineWrapper scriptEngine, List<String> localJavaImports) {
+
+		this.attributeConditions = new ArrayList<>();
+		this.involvedConditions = new HashMap<>();
+		this.assignedParameters = new HashSet<>();
 		this.scriptEngine = scriptEngine;
 
 		for (String condition : conditionParameters.keySet()) {
 			Collection<String> usedParameters = conditionParameters.get(condition);
-			AttributeCondition attCondition = new AttributeCondition(condition, usedParameters, scriptEngine);
-			attributeConditions.add(attCondition);
+			AttributeCondition attCondition = new AttributeCondition(condition, usedParameters, scriptEngine, localJavaImports);
+			this.attributeConditions.add(attCondition);
 
 			// Create a map for easy lookup of conditions a parameter is involved in:
 			for (String usedParameter : usedParameters) {
-				Collection<AttributeCondition> conditionList = involvedConditions.get(usedParameter);
+				Collection<AttributeCondition> conditionList = this.involvedConditions.get(usedParameter);
 				if (conditionList == null) {
-					conditionList = new ArrayList<AttributeCondition>();
-					involvedConditions.put(usedParameter, conditionList);
+					conditionList = new ArrayList<>();
+					this.involvedConditions.put(usedParameter, conditionList);
 				}
 				conditionList.add(attCondition);
 			}
 		}
+	}
+
+	public ConditionHandler(Map<String, Collection<String>> conditionParameters, ScriptEngine engine) {
+		this(conditionParameters, new ScriptEngineWrapper(engine, new String[0]), Collections.emptyList());
 	}
 
 	/**
@@ -75,9 +85,9 @@ public class ConditionHandler {
 	 */
 	public boolean setParameter(String paramName, Object value) {
 		boolean result = true;
-		if (assignedParameters.add(paramName)) {
-			scriptEngine.put(paramName, value);
-			Collection<AttributeCondition> conditionList = involvedConditions.get(paramName);
+		if (this.assignedParameters.add(paramName)) {
+			this.scriptEngine.put(paramName, value);
+			Collection<AttributeCondition> conditionList = this.involvedConditions.get(paramName);
 			if (conditionList != null) {
 				for (AttributeCondition condition : conditionList) {
 					condition.parameters.remove(paramName);
@@ -93,10 +103,10 @@ public class ConditionHandler {
 	 * @param paramName Parameter name.
 	 */
 	public void unsetParameter(String paramName) {
-		if (assignedParameters.remove(paramName)) {
-			Collection<AttributeCondition> conditionList = involvedConditions.get(paramName);
+		if (this.assignedParameters.remove(paramName)) {
+			Collection<AttributeCondition> conditionList = this.involvedConditions.get(paramName);
 			if (conditionList != null) {
-				for (AttributeCondition condition : involvedConditions.get(paramName)) {
+				for (AttributeCondition condition : this.involvedConditions.get(paramName)) {
 					condition.parameters.add(paramName);
 				}
 			}
@@ -109,7 +119,7 @@ public class ConditionHandler {
 	 * @return <code>true</code> if it is set.
 	 */
 	public boolean isSet(String paramName) {
-		return assignedParameters.contains(paramName);
+		return this.assignedParameters.contains(paramName);
 	}
 
 	/**
@@ -118,7 +128,7 @@ public class ConditionHandler {
 	 * @return The value.
 	 */
 	public Object getParameter(String paramName) {
-		return scriptEngine.get(paramName);
+		return this.scriptEngine.get(paramName);
 	}
 
 	/**
@@ -126,9 +136,9 @@ public class ConditionHandler {
 	 * @return Map with all parameter values.
 	 */
 	public Map<String, Object> getParameterValues() {
-		Map<String, Object> paramValues = new HashMap<String, Object>();
-		for (String paramName : assignedParameters) {
-			paramValues.put(paramName, scriptEngine.get(paramName));
+		Map<String, Object> paramValues = new HashMap<>();
+		for (String paramName : this.assignedParameters) {
+			paramValues.put(paramName, this.scriptEngine.get(paramName));
 		}
 		return paramValues;
 	}
