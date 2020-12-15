50ec2cf3736cd8b4cd7bbb1928302d829e2054f6
ruby <ruby@storm>
2020-12-15 19:10:24 +0100

    Add action to add current configuration to pcs of selected elements.

---
+++ plugins/variability/org.eclipse.emf.henshin.variability.configuration.ui/src/org/eclipse/emf/henshin/variability/configuration/ui/actions/AddElementToConfigurationAction.java
@@ -0,0 +1,42 @@
+package org.eclipse.emf.henshin.variability.configuration.ui.actions;
+
+import org.eclipse.emf.henshin.model.GraphElement;
+import org.eclipse.emf.henshin.model.ModelElement;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.variability.configuration.ui.helpers.VariabilityModelHelper;
+import org.eclipse.emf.henshin.variability.configuration.ui.providers.ConfigurationProvider;
+import org.eclipse.emf.henshin.variability.wrapper.VariabilityTransactionHelper;
+
+import configuration.Configuration;
+
+/**
+ * This class allows moving a selected element to a configuration; i.e. changing its presence condition according to the current configuration.
+ * 
+ * @author Stefan Schulz
+ *
+ */
+public class AddElementToConfigurationAction extends AbstractModifyPresenceConditionAction {
+
+	public final static String ID = "org.eclipse.emf.henshin.variability.ui.AddElementToConfigurationActionID";
+	
+	@Override
+	protected String getPresenceCondition(ModelElement modelElement) {
+		Rule rule = ((GraphElement) modelElement).getGraph().getRule();
+		Configuration configuration = ConfigurationProvider.getInstance().getConfiguration(rule);
+		String configurationPC = VariabilityModelHelper.getPresenceCondition(configuration);
+		String result = VariabilityTransactionHelper.INSTANCE.getPresenceCondition(modelElement);
+		
+		if (result == null || result.isEmpty()) {
+			result = configurationPC;
+		} else {
+			result = String.format("(%s) | (%s)", result, configurationPC);
+		}
+		
+		return result;
+	}
+
+	@Override
+	protected String getDialogTitle() {
+		return "Add to Configuration";
+	}
+}
