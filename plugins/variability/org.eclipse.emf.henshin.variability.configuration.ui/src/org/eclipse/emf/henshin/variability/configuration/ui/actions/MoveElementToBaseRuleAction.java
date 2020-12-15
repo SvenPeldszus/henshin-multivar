f170edf442a8bc936fc893dcca619daaca025af1
ruby <ruby@storm>
2020-12-15 18:02:19 +0100

    Show dialog only when any of the selected elements has a pc.

--- plugins/variability/org.eclipse.emf.henshin.variability.configuration.ui/src/org/eclipse/emf/henshin/variability/configuration/ui/actions/MoveElementToBaseRuleAction.java
+++ plugins/variability/org.eclipse.emf.henshin.variability.configuration.ui/src/org/eclipse/emf/henshin/variability/configuration/ui/actions/MoveElementToBaseRuleAction.java
@@ -25,11 +25,10 @@ public class MoveElementToBaseRuleAction implements IActionDelegate {
 
 	public final static String ID = "org.eclipse.emf.henshin.variability.ui.MoveElementToBaseRuleActionID";
 	private ArrayList<ModelElement> selectedModelElementList = new ArrayList<ModelElement>();
-	
-	
+
 	@Override
 	public void run(IAction action) {
-		if(selectedModelElementList != null && !selectedModelElementList.isEmpty()) {
+		if (selectedModelElementList != null && !selectedModelElementList.isEmpty()) {
 			List<String> changedElements = new ArrayList<String>();
 			for (ModelElement modelElement : selectedModelElementList) {
 				String pc = VariabilityTransactionHelper.INSTANCE.getPresenceCondition(modelElement);
@@ -37,35 +36,35 @@ public class MoveElementToBaseRuleAction implements IActionDelegate {
 					changedElements.add(modelElement.toString());
 				}
 			}
-			
-			MessageDialog messageDialog = new MessageDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-					"Move to Base Rule", null,
-					"The presence conditions of the following elements and all attached edges will be overwritten:\n" + String.join(", ", changedElements) + "\n\nDo you want to continue?",
-					MessageDialog.WARNING, new String[] { "Yes", "No" }, 0);
-			
-			
-			if (messageDialog.open() == 0) {				
-				for(ModelElement modelElement : selectedModelElementList) {
-					VariabilityTransactionHelper.INSTANCE.setPresenceCondition(modelElement, null);
-				}
+
+			if (!changedElements.isEmpty()) {
+				MessageDialog messageDialog = new MessageDialog(
+						PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Move to Base Rule", null,
+						"The presence conditions of the following elements and all attached edges will be overwritten:\n"
+								+ String.join(", ", changedElements) + "\n\nDo you want to continue?",
+						MessageDialog.WARNING, new String[] { "Yes", "No" }, 0);
+				if (messageDialog.open() != 0)
+					return;
+			}
+			for (ModelElement modelElement : selectedModelElementList) {
+				VariabilityTransactionHelper.INSTANCE.setPresenceCondition(modelElement, null);
 			}
 		}
-		
 	}
-		
+
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
 		selectedModelElementList.clear();
 		if (selection instanceof IStructuredSelection) {
-			Iterator<?> it = ((IStructuredSelection)selection).iterator();
-			
-			while(it.hasNext()) {
+			Iterator<?> it = ((IStructuredSelection) selection).iterator();
+
+			while (it.hasNext()) {
 				Object o = it.next();
 				if (o instanceof AbstractGraphicalEditPart) {
 					AbstractGraphicalEditPart editPart = (AbstractGraphicalEditPart) o;
-					if(editPart.getModel() instanceof View) {
+					if (editPart.getModel() instanceof View) {
 						View view = (View) editPart.getModel();
-						if(view.getElement() instanceof ModelElement) {
+						if (view.getElement() instanceof ModelElement) {
 							selectedModelElementList.add((ModelElement) view.getElement());
 						}
 					}
