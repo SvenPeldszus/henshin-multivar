075f531a2f30b6f31e654c6e0df3439aa727b64c
ruby <ruby@storm>
2020-12-15 17:54:13 +0100

    Add confirmation dialog when moving elements with pc to base rule.

--- plugins/variability/org.eclipse.emf.henshin.variability.configuration.ui/src/org/eclipse/emf/henshin/variability/configuration/ui/actions/MoveElementToBaseRuleAction.java
+++ plugins/variability/org.eclipse.emf.henshin.variability.configuration.ui/src/org/eclipse/emf/henshin/variability/configuration/ui/actions/MoveElementToBaseRuleAction.java
@@ -2,15 +2,18 @@ package org.eclipse.emf.henshin.variability.configuration.ui.actions;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.emf.henshin.model.ModelElement;
 import org.eclipse.emf.henshin.variability.wrapper.VariabilityTransactionHelper;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * This class enables moving a selected element to the base rule.
@@ -27,8 +30,24 @@ public class MoveElementToBaseRuleAction implements IActionDelegate {
 	@Override
 	public void run(IAction action) {
 		if(selectedModelElementList != null && !selectedModelElementList.isEmpty()) {
-			for(ModelElement modelElement : selectedModelElementList) {
-				VariabilityTransactionHelper.INSTANCE.setPresenceCondition(modelElement, null);
+			List<String> changedElements = new ArrayList<String>();
+			for (ModelElement modelElement : selectedModelElementList) {
+				String pc = VariabilityTransactionHelper.INSTANCE.getPresenceCondition(modelElement);
+				if (pc != null && !pc.isEmpty()) {
+					changedElements.add(modelElement.toString());
+				}
+			}
+			
+			MessageDialog messageDialog = new MessageDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+					"Move to Base Rule", null,
+					"The presence conditions of the following elements and all attached edges will be overwritten:\n" + String.join(", ", changedElements) + "\n\nDo you want to continue?",
+					MessageDialog.WARNING, new String[] { "Yes", "No" }, 0);
+			
+			
+			if (messageDialog.open() == 0) {				
+				for(ModelElement modelElement : selectedModelElementList) {
+					VariabilityTransactionHelper.INSTANCE.setPresenceCondition(modelElement, null);
+				}
 			}
 		}
 		
